6dd45d3dac5d515a2226e6c93def34d17f7a089a
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2025-05-21 17:56:48 -0700

    reading file

--- src/cobold/main.c
+++ src/cobold/main.c
@@ -1,6 +1,41 @@
-#include "common/orbit.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    printf("TODO: compile C code :3\n");
+#include "../common/str.h"
+
+#define eprintf(...) fprintf (stderr, __VA_ARGS__)
+#define bail(...) (eprintf(__VA_ARGS__), eprintf("\n"), exit(1))
+
+string file_read_to_string(string filepath) {
+    // must be null terminated, ugh
+    assert(strlen(filepath.raw) == filepath.len);
+    FILE *f = fopen(filepath.raw, "r");
+    if (!f) {
+        bail("Could not open file.");
+    }
+
+    fseek(f, 0, SEEK_END);
+    size_t len = ftell(f);
+    rewind(f);
+
+    string contents = string_alloc(len);
+    fread(contents.raw, 1, len, f);
+    return contents;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        char *my_name = argv[0];
+        if (!my_name) {
+            my_name = "cobold";
+        }
+        bail("Usage: %s <FILE>", my_name);
+    }
+    string filepath = str(argv[1]);
+    string src = file_read_to_string(filepath);
+    printf("source:\n```\n" str_fmt "```\n", str_arg(src));
+    bail("TODO: compile C code :3");
     return 0;
 }
